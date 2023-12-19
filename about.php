e79bb4f02493de51d41aae276880c5b7d8c67f34
Rohan Dafare <121180382+rohandafarefr@users.noreply.github.com>
2023-12-19 08:30:13 +0530

    update 2.0.1

--- about.php
+++ about.php
@@ -1,8 +1,46 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About - Dental Clinic Appointment Booking System</title>
     <link rel="stylesheet" href="./frontend/css/style.css">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f5f5f5;
+            color: #333;
+            margin: 0;
+            padding: 0;
+        }
+
+        main {
+            padding: 20px;
+        }
+
+        .about-section {
+            width: 80%;
+            margin: 0 auto;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            border-radius: 10px;
+            padding: 30px;
+            text-align: center;
+            margin-top: 20px;
+        }
+
+        h2 {
+            color: #333;
+            margin-bottom: 20px;
+        }
+
+        p {
+            margin-bottom: 20px;
+            font-size: 16px;
+            line-height: 1.6;
+        }
+
+    </style>
 </head>
 <body>
     <?php include 'header.php'; ?>
